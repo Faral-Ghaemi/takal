9d1f2b575311a18d7192735bed311c425852ecba
Mohammad Amin Ghaemi <40238905+Faral-Ghaemi@users.noreply.github.com>
2019-08-14 11:25:07 +0430

    wd

--- takalapp/views.py
+++ takalapp/views.py
@@ -45,6 +45,7 @@ def admin(request):
         'trips' : trips,
         'today': today,
         'scores': sum,
+        'onlinetrips': onlinetrips,
     }
     template = loader.get_template('index.html')
     return HttpResponse(template.render(context,request))
