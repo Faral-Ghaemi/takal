efdaaf3dbed31ee92a9cef71ba9c0b3880ac048b
Mohammad Amin Ghaemi <40238905+Faral-Ghaemi@users.noreply.github.com>
2019-08-14 11:19:30 +0430

    1

--- takal/urls.py
+++ takal/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from takalapp import views
-from rest_framework import routers
+
 
 
 urlpatterns = [
-    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
     path('a/', include('takalapp.urls'), name='takal'),
     path('api/register/', views.register, name='register'),
